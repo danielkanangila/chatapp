c22c0c8261d086ed4b21860ba43398b0ee4d5e33
Daniel Kanangila <daniel.kanangila@gmail.com>
2021-06-09 23:22:02 -0400

    added a middleware to validate the message body in order to make sure that the user can send a message only in the conversation that he/she is related to.

---
+++ server/permissions/index.js
@@ -0,0 +1,64 @@
+const { Op } = require("sequelize");
+const events = require("events");
+const { Conversation } = require("./../db/models");
+
+/**
+ * This middleware validate the message body, 
+ * and make sure that only one of the conversation owners can send message 
+ * in the conversation. Then set conversationId in request.body that will be used to create new message.
+ * @param {object} req | express request object
+ * @param {object} res | express response object
+ * @param {Function} next | express next callback
+ * @returns 
+ */
+const canSaveMessage = async (req, res, next) => {
+    const senderId = req.user.id;
+    const { conversationId, recipientId } = req.body;
+    let conversation;
+
+    if (conversationId) {
+        // Find conversation corresponding to the id  and senderId.
+        // This block make sure that only conversation owner can send message with this conversationId.
+        conversation = await Conversation.findOne({
+            where: {
+                id: conversationId,
+                [Op.and]: [{
+                    [Op.or]: [
+                        {
+                            user1Id: senderId,
+                        },
+                        {
+                            user2Id: senderId,
+                        },
+                    ]
+                }]
+            }
+        });
+        // if no conversation found, should return 401 error
+        if (!conversation)
+            return res.status(401).json({ error: "User not allowed to send message in this conversation or conversation id not found." });
+        // next handler
+        return next()
+    } else {
+        // if we don't have conversation id, find a conversation to make sure it doesn't already exist
+        conversation = await Conversation.findConversation(
+            senderId,
+            recipientId
+        );
+        // if no result create new conversation
+        if (!conversation) {
+            // create conversation
+            conversation = await Conversation.create({
+              user1Id: senderId,
+              user2Id: recipientId,
+            });
+        }
+        req.body.conversationId = conversation.id;
+        return next()
+    }
+
+}
+
+module.exports = {
+    canSaveMessage
+}
--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -2,40 +2,22 @@ const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
 const { isAuthenticated } = require("./../auth/middleware");
+const { canSaveMessage } = require("./../../permissions");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
-router.post("/", isAuthenticated, async (req, res, next) => {
+router.post("/", isAuthenticated, canSaveMessage, async (req, res, next) => {
   try {
     const senderId = req.user.id;
-    const { recipientId, text, conversationId, sender } = req.body;
+    // const conversationId = req.conversationId // This property is set in the request object after validation in canSaveMessage middleware.
+    const { text, sender, conversationId } = req.body;
 
-    // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
-    }
-    // if we don't have conversation id, find a conversation to make sure it doesn't already exist
-    let conversation = await Conversation.findConversation(
-      senderId,
-      recipientId
-    );
+    // if (onlineUsers.includes(sender.id)) {
+    //   sender.online = true;
+    // }
 
-    if (!conversation) {
-      // create conversation
-      conversation = await Conversation.create({
-        user1Id: senderId,
-        user2Id: recipientId,
-      });
-      if (onlineUsers.includes(sender.id)) {
-        sender.online = true;
-      }
-    }
-    const message = await Message.create({
-      senderId,
-      text,
-      conversationId: conversation.id,
-    });
-    res.json({ message, sender });
+    const message = await Message.create({ senderId, text, conversationId });
+    return res.json({ message, sender });
+
   } catch (error) {
     next(error);
   }
